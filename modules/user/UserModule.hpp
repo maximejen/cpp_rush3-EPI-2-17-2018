d8c9a3f4148872f2497a2653c82b94e26684882a
maxime.jenny <maxime.jenny@epitech.eu>
2018-01-20 12:59:14 +0000

    push UserModule, get all informations about the session, the user, his groups etc ..

--- modules/user/UserModule.hpp
+++ modules/user/UserModule.hpp
@@ -8,9 +8,30 @@
 #ifndef CPP_RUSH3_USERMODULE_HPP
 	#define CPP_RUSH3_USERMODULE_HPP
 
+	#include <string>
+	#include <AMonitorModule.hpp>
+	#include <vector>
 
-class UserModule {
+class UserModule : public AMonitorModule {
+public:
+	UserModule();
+	~UserModule();
+	const std::string &getUsername() const;
+	int getUid() const;
+	const std::string &getDefaultShell() const;
+	const std::string &getHomePath() const;
+	const std::vector<std::string> &getGroupNames() const;
+	const std::vector<gid_t> &getGroupGID() const;
+	int getSid() const;
 
+private:
+	std::string _username;
+	int _uid;
+	std::vector<gid_t> _groupGID;
+	std::vector<std::string> _groupNames;
+	int _sid;
+	std::string _homePath;
+	std::string _defaultShell;
 };
 
 
